69db545052a6abc89d47f8a68752e0ff5ea255a6
Donoshka <147371737+Donoshka@users.noreply.github.com>
2023-11-14 20:07:12 +0300

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -2,7 +2,7 @@ void problemSolution1(float consumed_water) {
     float cost;
     // write your code here
 float d = (consumed_water > 30) ? 30.0 : consumed_water;
-float e = (consumed water > 50) ? 20 : ((a == 30) ? (consumed_water - 30) : 0);
+float e = (consumed_water > 50) ? 20 : ((a == 30) ? (consumed_water - 30) : 0);
 float f = (consumed_water > 60) ? 10 : ((b == 20) ? (consumed_water - 50) : 0);
 
 cost += 13
