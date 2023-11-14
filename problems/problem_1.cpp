65e1a8810f1404cdc368bfdc973b2c87fe333d17
Donoshka <147371737+Donoshka@users.noreply.github.com>
2023-11-14 20:16:31 +0300

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,36 +1,15 @@
 void problemSolution1(float consumed_water) {
     // write your code here
-float cost = 13.0;
+float problemSolution1(float consumed_water)
+{
+    float cost;
+    float x = (consumed_water > 30) ? 30.0 : consumed_water;
+    float y = (consumed_water > 50) ? 20 : ((x == 30) ? (consumed_water - 30) : 0);
+    float z = (consumed_water > 60) ? 10 : ((y == 20) ? (consumed_water - 50) : 0);
 
-  if (consumed_water > 30.0) {
-    consumed_water = 30.0;
-  }
-  float d = consumed_water;
-  cost += d * 0.4;
-  if (consumed_water > 50.0) {
-    consumed_water = 20.0;
-  } else if (a == 30) {
-    consumed_water -= 30.0;
-  } else {
-    consumed_water = 0.0;
-  }
-  float e = consumed_water;
-  cost += e * 0.12;
-  if (consumed_water > 60.0) {
-    consumed_water = 10.0;
-  } else if (b == 20) {
-    consumed_water -= 50.0;
-  } else {
-    consumed_water = 0.0;
-  }
-  float f = consumed_water;
-  cost += f * 1.4;
-  if (consumed_water > 60.0) {
-    consumed_water -= 60.0;
-  } else {
-    consumed_water = 0.0;
-  }
-  cost += consumed_water * 1.5;
-  return cost;
+    cost += 13 + x * 0.4 + y * 0.12 + z * 1.4 + ((consumed_water - 60) > 0 ? (consumed_water - 60) : 0) * 1.5;
+    
+    return cost;
 }
+
 
